30934531a427815b0e0a72ab4061d7e98ec5b978
felipe soares siqueira <felipesoaressiqueira@gmail.com>
2025-02-06 21:49:41 -0300

    projeto

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,5 +8,34 @@ app = Flask(__name__)
 def home():
     return 'Olá, Flask'
 
+@app.route('/item', methods = ['POST'])
+
+def post_item():
+    data = request.get_json()
+    sql = f"INSERT INTO todolist(item, status) VALUES('{data['item']}','{data['status']}')"
+    banco(sql)
+    return data
+
+def banco(sql):
+    resultado = ""
+    try:
+        # Conexão com o banco de dados PostgreeSQL
+        conn = psycopg2.connect(
+            host = 'dpg-cuhul4bv2p9s73c22g80-a.oregon-postgres.render.com',
+            port = '5432',
+            dbname = 'senaidb_fhpm',
+            user = 'senaidb_fhpm_user',
+            password = 'F8kSUUFo7ZUgLut1vGGrqJTY9IhL4cCz'
+        )
+        cursor = conn.cursor() # cursor vai ser a variavel para executar os comandos SQL.
+        cursor.execute(sql) # executa o comando sql seja insert, select .. etc
+        cursor.close() #finaliza o cursor
+        conn.commit() # confirma o comando SQL
+        conn.close() # finaliza a conexão
+
+    except psycopg2.Error as e:
+        print("Erro na conexão do banco de dados")
+
+
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(debug=True)
